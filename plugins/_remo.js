c99838221de37b576fd18359b51b547dfe146435
YALLICO OWNER <yallico2024@gmail.com>
2026-07-14 11:24:46 -0500

    Actualizar _remo.js

--- plugins/_remo.js
+++ plugins/_remo.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted? m.quoted : m;
     let mime = (q.msg || q).mimetype || '';
 
-    if (!mime) throw `⚡ *Rayo Prem Bot* 🍓\n\nResponde a una imagen con *${usedPrefix + command}*`;
+    if (!mime) throw `👾 *Ricky Bot Prem*\n\nResponde a una imagen con *${usedPrefix + command}*`;
     if (!/image\/(jpe?g|png)/.test(mime)) {
         throw `⚠️ *Formato no soportado.* Solo JPG/PNG. Envía la imagen normal`;
     }
@@ -14,7 +14,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     const API_KEY = "FEx4CYmYN1QRQWD1mbZp87jV";
 
     await m.react('⏳');
-    await m.reply('⚡ *Quitando fondo modo rayo...*');
+    await m.reply('⚡ *Quitando fondo con Ricky Bot Prem...*');
 
     try {
         let img = await q.download();
@@ -22,11 +22,10 @@ let handler = async (m, { conn, usedPrefix, command }) => {
         if (img.length > 12 * 1024 * 1024) throw '❌ *Imagen muy pesada.* Máximo 12MB';
 
         let base64Img = img.toString('base64');
-        
+
         let form = new FormData();
         form.append('image_file_b64', base64Img);
         form.append('size', 'auto');
-        // QUITAMOS bg_color porque daba error
 
         let res = await fetch('https://api.remove.bg/v1.0/removebg', {
             method: 'POST',
@@ -46,8 +45,8 @@ let handler = async (m, { conn, usedPrefix, command }) => {
         await conn.sendFile(
             m.chat,
             processedImg,
-            'rayo_prem.png',
-            '✨ *Fondo eliminado con éxito* ✨\n\n⚡ *Rayo Prem Bot | Team Nightwish*',
+            'ricky_prem.png',
+            '✨ *Fondo eliminado con éxito* ✨\n\n👾 *Ricky Bot Prem*',
             m
         );
 
