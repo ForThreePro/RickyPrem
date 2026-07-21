9a0e7c3c0beaf8ba8b326ceedc4cd82e22df9926
YALLICO OWNER <yallico2024@gmail.com>
2026-07-21 10:09:28 -0500

    Actualizar group-n.js

--- plugins/group-n.js
+++ plugins/group-n.js
@@ -2,39 +2,50 @@ let mutedUsers = new Set();
 
 let handler = async (m, { conn, command, participants }) => {
     let mentionedJid = m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : false;
-    if (!mentionedJid) return m.reply(`╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
-│ 🔇 *SISTEMA DE MUTE*
+    if (!mentionedJid) return m.reply(`╭─⚡ *『 𝐑𝐈𝐂𝐊𝐘 𝐏𝐑𝐄𝐌 𝐁𝐎𝐓 』* ⚡
+│ 😎 *ERROR DE SISTEMA*
 │
-│ ⚡ *Etiqueta a una persona o responde a un mensaje*
+│ 🎯 *USO:* Responde a un mensaje
+│
+│ > *“Protocolo de muteo requiere objetivo”* ⚡
 ╰─────────────────❒`);
 
     let isUserAdmin = participants.find(p => p.id === mentionedJid)?.admin;
-    if (isUserAdmin) return m.reply(`╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
-│ ❌ *ERROR*
+    if (isUserAdmin) return m.reply(`╭─⚡ *『 𝐑𝐈𝐂𝐊𝐘 𝐏𝐑𝐄𝐌 𝐁𝐎𝐓 』* ⚡
+│ 🛡️ *ACCESO DENEGADO*
 │
-│ ⚡ *No puedes mutear a un administrador*
+│ ⚠️ *No se puede mutear a un administrador*
+│
+│ > *“Rango superior detectado”* 😎
 ╰─────────────────❒`);
-    if (mentionedJid === conn.user.jid) return m.reply(`╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
-│ ❌ *ERROR*
+
+    if (mentionedJid === conn.user.jid) return m.reply(`╭─⚡ *『 𝐑𝐈𝐂𝐊𝐘 𝐏𝐑𝐄𝐌 𝐁𝐎𝐓 』* ⚡
+│ 🤖 *PROTOCOLO FALLIDO*
 │
-│ ⚡ *No puedo mutearme a mi mismo*
+│ ⚠️ *No puedo realizar esta acción conmigo mismo*
+│
+│ > *“Autoeliminacion no permitida”* ❌
 ╰─────────────────❒`);
 
     if (command === "mute") {
         mutedUsers.add(mentionedJid);
-        conn.reply(m.chat, `╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
+        conn.reply(m.chat, `╭─⚡ *『 𝐑𝐈𝐂𝐊𝐘 𝐏𝐑𝐄𝐌 𝐁𝐎𝐓 』* ⚡
 │ 🔇 *USUARIO SILENCIADO*
 │
-│ ⚡ *@${mentionedJid.split('@')[0]}*
-│ 👾 *Sus mensajes serán eliminados*
+│ 👤 *Objetivo:* @${mentionedJid.split('@')[0]}
+│ 📊 *Estado:* MUTE ACTIVADO
+│
+│ > *“Se eliminaran sus mensajes”* ⚡
 ╰─────────────────❒`, m, { mentions: [mentionedJid] });
     } else if (command === "unmute") {
         mutedUsers.delete(mentionedJid);
-        conn.reply(m.chat, `╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
-│ 🔊 *USUARIO DESMUTEADO*
+        conn.reply(m.chat, `╭─⚡ *『 𝐑𝐈𝐂𝐊𝐘 𝐏𝐑𝐄𝐌 𝐁𝐎𝐓 』* ⚡
+│ 🔊 *USUARIO LIBERADO*
 │
-│ ⚡ *@${mentionedJid.split('@')[0]}*
-│ 👾 *Ya puede hablar de nuevo*
+│ 👤 *Objetivo:* @${mentionedJid.split('@')[0]}
+│ 📊 *Estado:* MUTE DESACTIVADO
+│
+│ > *“Puede volver a escribir”* 😎
 ╰─────────────────❒`, m, { mentions: [mentionedJid] });
     }
 };
@@ -50,11 +61,11 @@ handler.before = async (m, { conn, isAdmin }) => {
     }
 };
 
-handler.help = ['mute', 'unmute'].map(v => v + ' @user');
+handler.help = ['mute ( Reacciona Al Mensaje )', 'unmute ( Reacciona Al Mensaje )'].map(v => v + ' @user');
 handler.tags = ['grupos'];
 handler.command = /^(mute|unmute)$/i;
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
 
-export default handler;
+export default handler
